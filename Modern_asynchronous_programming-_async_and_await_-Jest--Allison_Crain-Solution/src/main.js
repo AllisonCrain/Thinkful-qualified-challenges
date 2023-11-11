5ffadc5041ae3041b947bd5b7650e4829901a5f3
Allison Crain <135656384+AllisonCrain@users.noreply.github.com>
2023-11-11 17:18:17 -0500

    Add files via upload

---
+++ Modern_asynchronous_programming-_async_and_await_-Jest--Allison_Crain-Solution/src/main.js
@@ -0,0 +1,25 @@
+const { welcome, goodbye, tell } = require("../utils/fortune-teller");
+
+async function getFortune(question) {
+  try{
+  const response = await tell(question);
+      console.log(`Your question was: ${question}`);
+      console.log(`Your fortune is: ${response}`);
+    }catch(err) {
+      console.log(`There was an error: ${err}`);
+    }
+}
+
+async function fullSession(question) {
+  try {
+  const response = await welcome();
+    console.log(response);
+    await getFortune(question);
+    console.log(await goodbye());
+  }
+    catch(err) {
+      console.log(`There was an error: ${err}`);
+    }
+}
+
+module.exports = { getFortune, fullSession };
