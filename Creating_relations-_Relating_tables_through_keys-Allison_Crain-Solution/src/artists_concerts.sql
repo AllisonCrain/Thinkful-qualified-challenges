026af03b250ff62594412925cf6f4bb1f1a01011
Allison Crain <135656384+AllisonCrain@users.noreply.github.com>
2023-12-07 21:47:51 -0500

    Add files via upload

---
+++ Creating_relations-_Relating_tables_through_keys-Allison_Crain-Solution/src/artists_concerts.sql
@@ -0,0 +1,9 @@
+CREATE TABLE artists_concerts
+(
+  artist_id INTEGER REFERENCES artists(artist_id),
+  concert_id INTEGER REFERENCES 
+concerts(concert_id),
+  scheduled_start_at TIME,
+  scheduled_end_at TIME,
+  PRIMARY KEY (artist_id, concert_id) -- Composite key
+);
